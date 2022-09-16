4f75ced6e9f04d311e8506d054e146a97ebf3d19
Ronnie Dong <ronniedong_79@hotmail.com>
2022-09-16 22:16:08 +1000

    header is completed version 1

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is a testing', 'https://media.danmurphys.com.au/dmo/product/140048-1.png?impolicy=PROD_SM')
+  ];
 
   constructor() { }
 
