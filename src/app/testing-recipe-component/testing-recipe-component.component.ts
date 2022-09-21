b37ab978a36421f5bd2de43e363d289e753f6acc
Ronnie Dong <ronniedong_79@hotmail.com>
2022-09-21 13:58:10 +1000

    recipe branch

---
+++ src/app/testing-recipe-component/testing-recipe-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testing-recipe-component',
+  templateUrl: './testing-recipe-component.component.html',
+  styleUrls: ['./testing-recipe-component.component.css']
+})
+export class TestingRecipeComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
