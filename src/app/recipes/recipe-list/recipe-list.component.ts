4d92b097f5e0c45f844aaefb2197e41b76d5df2d
Ronnie Dong <ronniedong_79@hotmail.com>
2022-09-21 13:49:57 +1000

    format code

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -5,18 +5,23 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a testing', 'https://media.danmurphys.com.au/dmo/product/140048-1.png?impolicy=PROD_SM'),
-    new Recipe('A test recipe', 'this is a testing', 'https://media.danmurphys.com.au/dmo/product/140048-1.png?impolicy=PROD_SM')
+    new Recipe(
+      'A test recipe',
+      'this is a testing',
+      'https://media.danmurphys.com.au/dmo/product/140048-1.png?impolicy=PROD_SM'
+    ),
+    new Recipe(
+      'A test recipe',
+      'this is a testing',
+      'https://media.danmurphys.com.au/dmo/product/140048-1.png?impolicy=PROD_SM'
+    ),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
